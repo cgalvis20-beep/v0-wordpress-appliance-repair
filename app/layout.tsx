3eb3f617aa4cd39ec0f1b674cedbb6a36067c011
v0agent <it+v0agent@vercel.com>
2026-06-29 13:48:45 +0000

    feat: add Google Tag Manager integration
    Added Google Tag Manager script and noscript iframe for tracking.
    
    Co-authored-by: cgalvis20-beep <236697006+cgalvis20-beep@users.noreply.github.com>

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,17 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-background">
       <head>
+        {/* Google Tag Manager */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NBW3XT4Z');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-NPL43N1W5Y"
           strategy="afterInteractive"
@@ -62,6 +73,16 @@ export default function RootLayout({
         </Script>
       </head>
       <body className="font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NBW3XT4Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Header />
         <main className="min-h-screen pb-20 lg:pb-0">{children}</main>
         <Footer />
